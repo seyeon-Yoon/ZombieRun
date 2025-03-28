81300b8ea06e586a452486dcf1cabc1d48c6e5d2
seyeon-Yoon <fjrkdlxm1000@gmail.com>
2025-03-28 17:27:03 +0900

    맵파일 수정요

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -66,7 +66,7 @@ router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
 router.get('/keypad', ctrl.output.keypad);
 router.get('/mnhome', ctrl.output.mnhome);
-router.get('/addTheme', ctrl.output.theme);
+router.get('/theme', ctrl.output.theme);
 router.get('/mapPopup', ctrl.output.mapPopup);
 router.get('/get-themes', ctrl.process.getThemes);
 router.get('/get-theme-data', ctrl.process.getThemeData);
@@ -74,7 +74,7 @@ router.get('/get-theme-data', ctrl.process.getThemeData);
 // API 라우트
 router.post('/login', ctrl.process.login);
 router.post('/verify-theme-code', ctrl.process.verifyThemeCode);
-router.post('/addTheme', upload.single('map_file'), ctrl.process.theme);
+router.post('/theme', upload.single('map_file'), ctrl.process.theme);
 router.post('/save-theme-data', ctrl.process.saveThemeData);
 router.post('/delete-theme', ctrl.process.deleteTheme);
 
